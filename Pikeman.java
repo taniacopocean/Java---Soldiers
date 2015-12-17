d113004fc1f065927a5a1b111642d38303e044ea
taniacopocean <tania_c1997@yahoo.co.uk>
2015-12-17 12:17:37 +0200

    Pikeman class

---
+++ Pikeman.java
@@ -0,0 +1,14 @@
+public class Pikeman extends Soldier{		//A class Pikeman which extends the Soldier class
+	public Pikeman(double a, double b, double s){
+	//The constructor of the class
+		super(a,b,s);
+
+	}
+
+	public String toString(){
+	//A toString method, to return the type of the Soldier (in this case Pikeman)
+		return "Pikeman";
+
+	}
+
+}
