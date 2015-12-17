5eb32ea6a484564a0b955e4e3d77ec9308cf402a
taniacopocean <tania_c1997@yahoo.co.uk>
2015-12-17 12:17:17 +0200

    Knight class

---
+++ Knight.java
@@ -0,0 +1,15 @@
+public class Knight extends Soldier{
+	//A class Knight which extends the Soldier class
+	public Knight(double a, double b, double s){
+	//The constructor of the class
+		super(a,b,s);
+	
+	}
+
+	public String toString(){
+	//A toString method, to return the type of the Soldier (in this case Knight)
+		return "Knight";
+
+	}
+
+}
